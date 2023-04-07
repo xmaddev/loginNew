aec4920bfad2a400aded00abce8c0a229efd7d3f
xmaddev <64865926+xmaddev@users.noreply.github.com>
2023-04-07 20:52:33 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-[
+{[
     { id: 0, name: "Creola Katherine Johnson", profession: "mathematician" },
     { id: 1, name: "Mario José Molina-Pasquel Henríquez", profession: "chemist" },
     { id: 2, name: "Mohammad Abdus Salam", profession: "physicist" },
@@ -7,5 +7,5 @@
     { id: 5, name: "Subrahmanyan Chandrasekhar", profession: " astrophysicist" },
     { id: 6, name: "qw Chandrasekhar", profession: " astrophysicist" },
     { id: 7, name: "qw qe", profession: " astrophysicist" },
-]
+]}
   

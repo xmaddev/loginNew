499e00e9c9e38cce437dadad28ad244d6a792c51
xmaddev <64865926+xmaddev@users.noreply.github.com>
2023-04-08 00:01:58 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 {
-    "data": {
+    "data": {[
         { "id": 0, "name": "Creola Katherine Johnson", "profession": "mathematician" },
         { "id": 1, "name": "Mario José Molina-Pasquel Henríquez", "profession": "chemist" },
         { "id": 2, "name": "Mohammad Abdus Salam", "profession": "physicist" },
@@ -8,6 +8,6 @@
         { "id": 5, "name": "Subrahmanyan Chandrasekhar", "profession": "astrophysicist" },
         { "id": 6, "name": "qw Chandrasekhar", "profession": "astrophysicist" },
         { "id": 7, "name": "qw qe", "profession": "astrophysicist" },
-    }
+    ]}
 }
   

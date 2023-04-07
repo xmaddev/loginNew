893afac886f1bfe37a61ec8a90ab8c32c941af21
xmaddev <64865926+xmaddev@users.noreply.github.com>
2023-04-08 00:12:58 +0300

    Update data.js

--- data.js
+++ data.js
@@ -6,8 +6,6 @@
         { "id": "3", "name": "Percy Lavon Julian", "profession": "chemist" },
         { "id": "4", "name": "Subrahmanyan Chandrasekhar", "profession": "astrophysicist" },
         { "id": "5", "name": "Subrahmanyan Chandrasekhar", "profession": "astrophysicist" },
-        { "id": "6", "name": "qw Chandrasekhar", "profession": "astrophysicist" },
-        { "id": "7", "name": "qw qe", "profession": "astrophysicist" }
     ]
 }
   

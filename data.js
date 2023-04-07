59cb571b257ab14ea063942762c8f6e83c644664
xmaddev <64865926+xmaddev@users.noreply.github.com>
2023-04-08 00:07:29 +0300

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,7 @@
         { "id": "4", "name": "Subrahmanyan Chandrasekhar", "profession": "astrophysicist" },
         { "id": "5", "name": "Subrahmanyan Chandrasekhar", "profession": "astrophysicist" },
         { "id": "6", "name": "qw Chandrasekhar", "profession": "astrophysicist" },
-        { "id": "7", "name": "qw qe", "profession": "astrophysicist" },
+        { "id": "7", "name": "qw qe", "profession": "astrophysicist" }
     ]
 }
   
